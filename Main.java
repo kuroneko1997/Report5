02421dfa6d97752cf64520e247dab174daaf97ee
toguchi takeryu <e165741@ie.u-ryukyu.ac.jp>
2016-12-12 19:13:37 +0900

    プログラミングレポート５

---
+++ Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165741.Report5;
+
+/**
+ * Created by e1657 on 2016/12/12.
+ */
+public class Main {
+    public static void main(String[] ages){
+        String str = null;
+        System.out.printf("%s",str.length());
+    }
+}
